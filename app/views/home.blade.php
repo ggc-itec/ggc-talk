5b000bdd611f4559879c42ae38c5b3f1d6678de0
tacksoo <tacksoo.im@gmail.com>
2013-12-13 22:13:22 -0500

    hello world front page added

---
+++ app/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ggc-talk</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css'); }}"> 
+    <script src="{{ asset('js/jquery-2.0.3.min.js'); }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js'); }}"></script>
+  </head>
+
+
+  <body>
+    Hello World
+  </body>
+
+</html>
+
