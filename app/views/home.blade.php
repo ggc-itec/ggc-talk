4029a8ed72a8766820c8bf03db46fdea054c71de
tacksoo <tacksoo.im@gmail.com>
2013-12-14 21:09:04 -0500

    home page update

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,14 +2,37 @@
 <html>
   <head>
     <title>ggc-talk</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css'); }}"> 
+    <link red="shortcut icon" href="">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css'); }}" rel="stylesheet"> 
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css'); }}" rel="stylesheet"> 
     <script src="{{ asset('js/jquery-2.0.3.min.js'); }}"></script>
     <script src="{{ asset('js/bootstrap.min.js'); }}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
-
+  <!-- twitter bootstrap sample code  http://getbootstrap.com/examples/starter-template/# -->
   <body>
-    Hello World
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">GGC Talk</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+ 
   </body>
 
 </html>
