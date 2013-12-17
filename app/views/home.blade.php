8b6171124356a2a37d4fecf9bd1507ca932860c5
tacksoo <tacksoo.im@gmail.com>
2013-12-16 20:43:08 -0500

    modified navbar

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,9 +2,8 @@
 <html>
   <head>
     <title>ggc-talk</title>
-    <link red="shortcut icon" href="">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css'); }}" rel="stylesheet"> 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css'); }}" rel="stylesheet"> 
+    <link rel="shortcut icon" href="{{ asset('img/ggctalk-small.png'); }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css'); }}" rel="stylesheet"> 
     <script src="{{ asset('js/jquery-2.0.3.min.js'); }}"></script>
     <script src="{{ asset('js/bootstrap.min.js'); }}"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +11,7 @@
 
   <!-- twitter bootstrap sample code  http://getbootstrap.com/examples/starter-template/# -->
   <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="navbar navbar-ggc navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -26,8 +25,19 @@
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">App 1</a></li>
+            <li><a href="#about">App 2</a></li>
+            <li><a href="#about">App 3</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="dropbdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                 Developers
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="http://168.28.21.90:1080/" target="_blank">Redmine</a></li>
+                <li><a href="https://github.com/ggc-itec/ggc-talk" target="_blank">Github</a></li>              
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
