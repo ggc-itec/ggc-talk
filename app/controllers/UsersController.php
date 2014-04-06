7395638858cfc355219f4cce308a18f09a21b683
jordan-r-smith <jordan.r.smith@gmail.com>
2014-04-06 14:52:46 -0400

    Updated README and added additional comments.

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,11 +1,18 @@
 <?php
 
+/*
+ * This is the controller for the User model.
+ * 
+ */
+
 class UsersController extends BaseController {
 
+  // Create login view
   public function showLogin() {
     return View::make('auth.login');
   }
 
+  // Attempt to login a user
   public function login() {
     $user = array('id' => Input::get('email'), 'password' => Input::get('password'));
 
@@ -16,15 +23,18 @@ class UsersController extends BaseController {
     }
   }
 
+  // Logout a logged in user
   public function logout() {
     Auth::logout();
     return Redirect::route('home') -> with(array('alert' => 'You are successfully logged out.', 'alert-class' => 'alert-success'));
   }
 
+  // Create register view
   public function showRegister() {
     return View::make('auth.register');
   }
 
+  // Attempt to register a new user
   public function register() {
     $first_name = Input::get('first_name');
     $last_name = Input::get('last_name');
@@ -32,13 +42,14 @@ class UsersController extends BaseController {
     $password = Input::get('password');
     $confirm_password = Input::get('confirm_password');
     
-    
     $regex = preg_match('/\b\w*(@ggc.edu)\b/', $email);
 
+    // If the email does not end with a @ggc.edu email address
     if (!$regex) {
       return Redirect::route('register') -> with(array('alert' => 'Not a valid @ggc.edu email address!', 'alert-class' => 'alert-danger'));
     }
 
+    // Attempt to saved a new user to the database
     if (strcmp($password, $confirm_password) == 0) {
       $hashed_password = Hash::make($password);
       try {
@@ -52,6 +63,7 @@ class UsersController extends BaseController {
         return Redirect::route('register') -> with(array('alert' => 'Error: Failed to register user in database.', 'alert-class' => 'alert-danger'));
       }
 
+      // Login the new user
       $user = User::where('id', $email) -> first();
       Auth::login($user);
 
