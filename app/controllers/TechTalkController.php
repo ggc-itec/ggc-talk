5c924e1cc9cdc2c254008db609f61f6704ddf274
Robert Curtis <rcurtis@ggc.edu>
2014-04-09 20:08:11 -0400

    Added new template and migration.

---
+++ app/controllers/TechTalkController.php
@@ -0,0 +1,19 @@
+<?php
+
+class TechTalkController extends BaseController
+{
+    public function index()
+    {
+        return View::make('techtalks.index');
+    }
+
+    public function showFavs()
+    {
+
+    }
+
+    public function handleAdd()
+    {
+
+    }
+}
---
+++ app/views/techtalks/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<div class="jumbotron">
+    <h2>Quote of the Day</h2>
+    <p>
+        This is the index.blade for the PostController
+    </p>
+</div>
+@stop
+
+
