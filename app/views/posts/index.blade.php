a8e4d0a6604abd1d4e696d3ff4d601e46a368b7c
Joe O <josepho011235@gmail.com>
2014-04-22 23:36:25 -0400

    small change to index

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -10,4 +10,14 @@
   <div class="panel-body">
     
   </div>
+
 </div>
+
+@foreach ($posts as $post)
+    <p>This is user {{ $post->id }}</p>
+    <div class="form-group">
+    <label for="comment">Comment</label>
+    <input type="text" class="form-control" name="comment" />
+  </div>
+    
+@endforeach
