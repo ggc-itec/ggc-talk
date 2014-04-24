6997f9985cfca396d4ec63b3f3657cc041a98ced
Joe O <josepho011235@gmail.com>
2014-04-24 00:35:42 -0400

    adding some more details to the Category View

--- app/views/categories/index.blade.php
+++ app/views/categories/index.blade.php
@@ -5,17 +5,17 @@
 <div class="page-header">
   <h1>GGC Forum - Categories</h1>
 </div>
-
 <div class="panel panel-default">
   <div class="panel-body">
-    
+		@foreach ($categories as $category)
+     <p>
+     	ID:    {{ $category->id }}	
+     	<br>
+    	Title: {{ $category->title }}	
+	</p> 
+	<br>    
+@endforeach    
   </div>
-
 </div>
-
-@foreach ($categories as $category)
-    <p>{{ $category->id }}</p>
-@endforeach
 </div>
-
 @stop
