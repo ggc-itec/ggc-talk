711261d88507e5b3361b78aaa3e840f821611193
Joe O <josepho011235@gmail.com>
2014-04-28 19:18:34 -0400

    fixed unopened div on view

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -7,22 +7,23 @@
 </div>
 <div class="panel panel-default">
   <div class="panel-body">   
+   <div class="thread">
+                <div class="title">
+                  <h3>header</h3>
+                </div>
     @if (count($posts) > 0)	
     @foreach ($posts as $post)
-    <div class="thread">
-      <div class="title">
-        <h3>{{ $post->topic->title}}</h3>
-      </div>
+   
       <div class='Message'>
-        <p><span class=""> message: {{ $post->message}}</span></p>         
+          <p><span class=""> message: {{ $post->message}}</span></p>         
 
-        <div class="userInfo">
-          <h6><a href="">name: {{  $post->temp_username  }}</a></h6>
-          <ul class="">
-            <li>  {{  Dates::showTimeAgo($post->created_at)}}</li>
-          </ul>      
-        </div>
-        <a class="btn btn-primary" href="{{ action('PostController@create') }}"> comment </a>
+          <div class="userInfo">
+            <h6><a href="">name: {{  $post->temp_username  }}</a></h6>
+            <ul class="">
+              <li>  {{  Dates::showTimeAgo($post->created_at)}}</li>
+            </ul>      
+          </div>
+          <a class="btn btn-primary" href="{{ action('PostController@create') }}"> comment </a>
       </div>
       @endforeach
       @else
