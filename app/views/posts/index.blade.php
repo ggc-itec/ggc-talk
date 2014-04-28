eef419db1aa232b5e731aa4fd2f7679a1da529ee
Joe O <josepho011235@gmail.com>
2014-04-28 19:28:33 -0400

    added index for posts

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -12,20 +12,46 @@
                   <h3>header</h3>
                 </div>
     @if (count($posts) > 0)	
+   <h3>Main Posts</h3>
+  <table class="topicTable ">
+      <thead>
+        <th> <h4 class="title"><span>Title </span><a class="colspan"></a></h4> </th>
+        <th> <h4 class="title"><span>Description </span><a class="colspan"></a></h4> </th>
+        <th> <h4 class="title"><span>Category </span><a class="colspan"></a></h4> </th>
+        <th> <h4 class="title"><span>Created </span><a class="colspan"></a></h4> </th>
+      </thead>
+      <tbody class="table-striped">
+        @if (count($posts) > 0)  
+        @foreach ($posts as $post)
+        <tr>    
+          <h3>{{ HTML::linkAction('PostController@show', $post->title, array($post->id)) }} </h3>
+          <td>{{  $post->temp_username  }}</td>
+          <td> {{ $post->topic->title }}  </td>
+          <td>{{  Dates::showTimeAgo($post->created_at)}}</td>
+        </tr>
+
+        @endforeach
+        @else
+        <p> No topics :( </p>
+        @endif 
+      </tbody>  
+    </table> 
+
+ <!-- 
     @foreach ($posts as $post)
-   
-      <div class='Message'>
+    <div class='Message'>
           <p><span class=""> message: {{ $post->message}}</span></p>         
 
           <div class="userInfo">
             <h6><a href="">name: {{  $post->temp_username  }}</a></h6>
             <ul class="">
-              <li>  {{  Dates::showTimeAgo($post->created_at)}}</li>
+              <li>  </li>
             </ul>      
           </div>
           <a class="btn btn-primary" href="{{ action('PostController@create') }}"> comment </a>
       </div>
-      @endforeach
+      @endforeach -->
+
       @else
       <p> No Posts Found :( </p>
       @endif  
