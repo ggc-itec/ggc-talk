731377bb3e9e2e827f233fc16cf2cd60f0326f1f
James Timothy McCravy <jmccravy@ggc.edu>
2014-05-09 10:17:12 -0400

    One thing

---
+++ app/database/migrations/2014_05_09_101341_create_jokes_table.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJokesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jokes', function($table) {
+      $table->increments('id');
+      $table->string('joke', 500);
+      $table->string('laughtrack',50);
+    });
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'So two guys walk into a bar... You\'d think one of them would have seen it coming.',
+      'laughtrack' => 'Ohohohoho!'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'What did the mother Buffalo say when her boy left for college? BYE-SON!',
+      'laughtrack' => '*groooaaan*'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'At first Bob didn\'t like his new haircut, but it started to grow on him.',
+      'laughtrack' => 'Ahahahaha!'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'Why shouldn\'t you write with a broken pencil? BECAUSE IT\'S POINTLESS',
+      'laughtrack' => 'Bwahahahaha!'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'They say that maturity is learning how to grow up, go to college, get a job, and then join in on this great mysterious adventure known as adulthood. Well, to that I say *PPTTTHHHH*',
+      'laughtrack' => '*snrk* Heh.'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'A baby seal walks into a club...',
+      'laughtrack' => '...Huh? Oh wait, I get it!'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'Two atoms are walking down the street together. One turns and says, "Hey, you just stole an electron from me!" "Are you sure?" asks the second atom. To which the first atom replies, Yeah, I\'m positive!',
+       'laughtrack' => 'Heheheh. Heheh.'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'A neutron walks into a bar and asks "how much for a beer?" The bartender says, "for you? no charge."',
+      'laughtrack' => 'Pffft, hahaha!'
+    ));
+    
+    DB::table('jokes')->insert(array(
+      'joke' => 'There’s two fish in a tank. One turns to the other and says ‘You man the guns, I’ll drive’',
+      'laughtrack' => 'GWAHAHAHAHAHAHA!!'
+    ));
+    }
+
+	/**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    Schema::drop('jokes');
+    }
+
+}
